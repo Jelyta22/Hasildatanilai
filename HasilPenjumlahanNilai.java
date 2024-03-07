586e50b2f5b514642e4cfc2d02c246b965bb1443
Jelyta22 <162581676+Jelyta22@users.noreply.github.com>
2024-03-07 19:20:41 +0700

    Add files via upload
    tugas dengan matkul (PBO)

---
+++ HasilPenjumlahanNilai.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hasilpenjumlahannilai;
+
+/**
+ *
+ * @author USER
+ */
+public class HasilPenjumlahanNilai{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       int nilai1, nilai2, nilai3, jumlah, mod;
+    
+   nilai1=80;
+   nilai2=90;
+   nilai3=75;
+   
+   jumlah = nilai1 + nilai2 + nilai3;
+   
+   mod=nilai1 + nilai2 + nilai3;
+   
+   System.out.println("hasil penjumlahan nilai1 + nilai2 + nilai3= 80+90+75 =" +jumlah);
+    
+
+    
+
+   
+          
+   
+    }   
+            
+     
+}       
+                    
+           
+                     
+             
+    
+
